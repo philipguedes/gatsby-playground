5b8b5b8d53c50ecfce0ee4f8e18a06f0a11f065e
Philip Guedes <philipguedes@gmail.com>
2021-09-13 23:36:33 -0300

    Add tsconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,19 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Playground",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Guedes e Lima Advocacia e Consultoria',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud",
-  "@chakra-ui/gatsby-plugin",
-  {
-    resolve: `gatsby-plugin-typescript`,
-    options: {
-      isTSX: true, // defaults to false
-      jsxPragma: `jsx`, // defaults to "React"
-      allExtensions: true, // defaults to false
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    '@chakra-ui/gatsby-plugin',
+    `gatsby-plugin-tsconfig-paths`,
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
     },
-  }],
+  ],
 };
